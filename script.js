66c834d80e4b3333314d53ef1d74bdbc9daa4750
Maruigi <fahadmui123@gmail.com>
2025-04-01 17:29:58 -0400

    Validation update

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+//For the validation of the contact information form
+const form = document.getElementById('formRegistration');
+
+const nameInput = document.getElementById('name');
+const usernameInput = document.getElementById('username');
+
+const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+
+const successMessage = document.getElementById('successMessage');
+const clearMessage = document.getElementById('clear-btn');
+
+form.addEventListener('submit', function(event) {
+    nameError.textContent = '';
+    usernameError.textContent = '';
+    successMessage.textContent = ''; 
+
+    let isValid = true;
+
+    if (nameInput.value.trim() === '') {
+        nameError.textContent = 'Please enter the title of your post';
+        event.preventDefault();  //Prevents form submission
+        isValid = false;
+    }
+
+    if (usernameInput.value.trim() === '') {
+        usernameError.textContent = 'Please enter the content of your post';
+        event.preventDefault();  //Prevents form submission
+    isValid = false;
+    }
+
+    //If all inputs are valid, show success message
+    if (isValid) {
+        successMessage.textContent = 'The post has been added successfully!';
+    }
+
+    event.preventDefault(); 
+});
+
 document.getElementById('formRegistration').addEventListener('submit', function(s) {
     s.preventDefault();
     const blogTitle = document.getElementById('name').value;
